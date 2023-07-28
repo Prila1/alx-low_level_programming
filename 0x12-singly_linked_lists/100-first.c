3b02a772f7b8eb8ec7aad877d525c101fdc79bd0
Prila1 <yarteypriscy@gmail.com>
2023-07-28 16:26:30 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include "lists.h"
 
-void start(void) __attribute__((constructor));
+void premain(void) __attribute__((constructor));
 
 /**
  * premain - prints amessage before the main function
  * This function uses the constructor attribute to run before main
  */
-	void start(void)
+	void premain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
