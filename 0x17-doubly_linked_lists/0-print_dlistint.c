691a546d03cfc9169d8d6957b4af5b8e913463b4
Prila1 <yarteypriscy@gmail.com>
2023-09-15 08:52:44 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all the nodes of ll
+ * @h: Is the pointer to node structure
+ * Return: the amount of nodes
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
